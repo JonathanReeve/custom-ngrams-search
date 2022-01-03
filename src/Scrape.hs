a71b1460f3061b47d1c089e8be4ad39c9dd421ea
Jonathan Reeve <jonathan.reeve@columbia.edu>
2022-01-03 10:09:24 -0500

    annotate scraping module

--- src/Scrape.hs
+++ src/Scrape.hs
@@ -1,5 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+-- Scrapes start and end bounds for Ngrams zip files.
+-- E.g., in the 2-grams index, http://storage.googleapis.com/books/ngrams/books/20200217/eng/eng-2-ngrams_exports.html
+-- the file at http://storage.googleapis.com/books/ngrams/books/20200217/eng/2-00039-of-00589.gz
+-- starts "155_NUM counts_NOUN" and ends (implicitly) before the next one,
+-- 163_NUM 2.90. So we get all those bounds.
+-- Usage: runhaskell Scrape
+
 module Scrape where
 
 import Network.HTTP.Client.Conduit ( getUri, Request )
@@ -22,11 +29,13 @@ findEnds ((url, start), (url', start')) = WordBounds { zipFile = url, start = C8
 
 main = do
   let urls = ["http://storage.googleapis.com/books/ngrams/books/20200217/eng-fiction/eng-fiction-"
-              <> show n <> "-ngrams_exports.html" | n <- [1..5]]
+              <> show n <> "-ngrams_exports.html" | n <- [1..3]]
   let url = urls !! 1
   links <- mapM scrape urls
   let links' = concat links -- flatten
+  -- print links
   let zipped = zip links' (tail links')
   let bounds = map findEnds zipped
   let boundsEncoded = encodeJSON bounds
   BS.writeFile "bounds.json" (UTF.fromString boundsEncoded)
+  -- print boundsEncoded
